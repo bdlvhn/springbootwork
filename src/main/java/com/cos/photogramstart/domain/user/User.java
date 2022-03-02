e8e20a2468420f1061c480d24e1c4eb3cb31e631
bdlvhn <bdlvhn@outlook.com>
2022-03-02 23:05:54 +0900

    first commit

---
+++ src/main/java/com/cos/photogramstart/domain/user/User.java
@@ -0,0 +1,68 @@
+package com.cos.photogramstart.domain.user;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.PrePersist;
+
+import com.cos.photogramstart.domain.image.Image;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+// JPA - Java Persistence API (자바로 데이터를 영구적으로 저장(DB)할 수 있는 API를 제공)
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity // DB에 테이블을 생성
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // 번호 증가 전략이 데이터베이스를 따라간다.
+	private int id;
+	
+	@Column(length = 20, unique = true)
+	private String username;
+	@Column(nullable = false)
+	private String password;
+	@Column(nullable = false)
+	private String name;
+	private String website; // 웹사이트
+	private String bio; // 자기소개
+	@Column(nullable = false)
+	private String email;
+	private String phone;
+	private String gender;
+	
+	private String profileImageUrl;
+	private String role; // 권한
+	
+	// 나는 연관관계의 주인이 아니다. 그러므로 테이블에 컬럼을 만들지 마.
+	// User를 SELECT할 때 해당 UserId로 등록된 image를 다 가져와
+	// Lazy = User를 select할 때 해당 userId로 등록된 image를 가져오지 마. 대신 getImages() 함수의 image가 호출될 때 가져와.
+	// Eager = User를 select할 때 해당 UserId로 등록된 image를 전부 join해서 가져와
+	@OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
+	@JsonIgnoreProperties({"user"})
+	private List<Image> images;
+	
+	private LocalDateTime createDate;
+	
+	@PrePersist // DB에 INSERT 되기 직전에 실행
+	public void createDate() {
+		this.createDate = LocalDateTime.now();
+	}
+	
+	
+}
